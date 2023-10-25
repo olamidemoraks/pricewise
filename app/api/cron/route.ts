e26f03ea1aef20232cab7a3ab1a81f614e1862a1
olamidemoraks <olamidemoraks@gmail.com>
2023-10-25 15:21:54 +0100

    fix build errors2

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -11,7 +11,7 @@ import {
 import { profile } from "console";
 import { NextResponse } from "next/server";
 
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
